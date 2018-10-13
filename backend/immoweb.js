67d7d6fedd021a03f2b6759ac1fd3c5aa2447fdb
Larry Boltovskoi <larry.bolt@gmail.com>
2018-10-13 20:48:48 +0200

    Add eslint and ran eslint-fix

--- backend/immoweb.js
+++ backend/immoweb.js
@@ -1,29 +1,27 @@
 const request = require('request-promise');
 const querystring = require('querystring');
+const polyline = require('@mapbox/polyline');
 const api = require('./api.json');
 
-const polyline = require('@mapbox/polyline');
 
 // coordinates is a [latitude, longitude]
 exports.getClassifieds = async (coordinates) => {
-    const params = {
-        propertyTypes: "HOUSE", //required
-        transactionTypes: "FOR_RENT", //required
-        minBedroomCount: 2,
-        maxPrice: 1500,
-        geoSearchPoint: polyline.encode([coordinates]),
-        geoSearchRadius: 10000,
-        range: "0-30"
-    };
+  const params = {
+    propertyTypes: 'HOUSE', // required
+    transactionTypes: 'FOR_RENT', // required
+    minBedroomCount: 2,
+    maxPrice: 1500,
+    geoSearchPoint: polyline.encode([coordinates]),
+    geoSearchRadius: 10000,
+    range: '0-30',
+  };
 
-    if (api.debug) console.log('GET', api.immoweb_url + "/classifieds?" + querystring.stringify(params));
-    return request({
-        url: api.immoweb_url + "/classifieds?" + querystring.stringify(params),
-        headers: {
-            "x-iw-api-key": api.immoweb_key,
-            Accept: "application/vnd.be.immoweb.classifieds.v2.1+json" 
-        }
-    }).then(body => {
-        return JSON.parse(body);
-    });
+  if (api.debug) console.log('GET', `${api.immoweb_url}/classifieds?${querystring.stringify(params)}`);
+  return request({
+    url: `${api.immoweb_url}/classifieds?${querystring.stringify(params)}`,
+    headers: {
+      'x-iw-api-key': api.immoweb_key,
+      Accept: 'application/vnd.be.immoweb.classifieds.v2.1+json',
+    },
+  }).then(body => JSON.parse(body));
 };
--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,44 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const google = require('./google');
 const immoweb = require('./immoweb');
 
 const app = express();
-app.options("*", cors());
+app.options('*', cors());
 
 // GET /get-location?input=Brussels
-app.get("/get-location", async (req, res) => {
-	if (!req.query.input || req.query.input.length === 0)
-		return res.send("please specify the input");
-	const input = req.query.input;
+app.get('/get-location', async (req, res) => {
+  if (!req.query.input || req.query.input.length === 0) return res.send('please specify the input');
+  const input = req.query.input;
 
-	const inputCoordinates = await google.getCoordinatesFromText(input);
-	const listOfHouses = await immoweb.getClassifieds(inputCoordinates);
+  const inputCoordinates = await google.getCoordinatesFromText(input);
+  const listOfHouses = await immoweb.getClassifieds(inputCoordinates);
 
-	// filter those without geopoint
-	const filtered = listOfHouses.filter(h => h.property.location.hasOwnProperty("geoPoint"));
+  // filter those without geopoint
+  const filtered = listOfHouses.filter(h => h.property.location.hasOwnProperty('geoPoint'));
 
-	const promiseFiltered = filtered.map(async item => {
-			// do the google directions api call here to get duration
-			itemCoordinates = [
-				item.property.location.geoPoint.latitude,
-				item.property.location.geoPoint.longitude,
-			];
-			item.travelDuration = {
-				driving: await google.getDuration(inputCoordinates, itemCoordinates, 'driving'),
-				walking: await google.getDuration(inputCoordinates, itemCoordinates, 'walking'),
-			};
-			return item;
-		});
-	const results = await Promise.all(promiseFiltered);
+  const promiseFiltered = filtered.map(async (item) => {
+    // do the google directions api call here to get duration
+    itemCoordinates = [
+      item.property.location.geoPoint.latitude,
+      item.property.location.geoPoint.longitude,
+    ];
+    item.travelDuration = {
+      driving: await google.getDuration(inputCoordinates, itemCoordinates, 'driving'),
+      walking: await google.getDuration(inputCoordinates, itemCoordinates, 'walking'),
+    };
+    return item;
+  });
+  const results = await Promise.all(promiseFiltered);
 
-	const cleanResults = results
-		.filter(item => item.travelDuration != '-1') // only include those that we can find a route to
-		.sort((a, b) => parseInt(a.travelDuration.driving) - parseInt(b.travelDuration.driving)) // sort them by lowest travelroute
+  const cleanResults = results
+    .filter(item => item.travelDuration != '-1') // only include those that we can find a route to
+    .sort((a, b) => parseInt(a.travelDuration.driving) - parseInt(b.travelDuration.driving)); // sort them by lowest travelroute
 
-	res.send(cleanResults);
+  res.send(cleanResults);
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
 });

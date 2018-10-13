ba45b1c7e8a8f20d5180c5f4558beb73d14ba8a8
Carolyne Peelman <peelmancarolyne@gmail.com>
2018-10-13 18:04:51 +0200

    Small lay-out fix

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -10,70 +10,15 @@ import { ImmoWebService } from '@app/immoweb.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnChanges {
-  quote: string;
-  isLoading: boolean;
   workLocation: string;
   maxDuration = 40;
   houses: any[];
 
   constructor(private quoteService: QuoteService, private immoWebService: ImmoWebService) {}
 
-  ngOnInit() {
-    this.isLoading = true;
-    this.quoteService
-      .getRandomQuote({ category: 'dev' })
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe((quote: string) => {
-        this.quote = quote;
-      });
-  }
+  ngOnInit() {}
 
   ngOnChanges() {
-    this.houses = [
-      {
-        Type: 'Bungalow',
-        City: 'Wolkrange',
-        PostalCode: 6780,
-        Bedrooms: 2,
-        Size: 173,
-        Price: 260000,
-        Duration: 20,
-        Image: '../../assets/housie1.jpg',
-        Info:
-          "LORRAINE : Province de Luxembourg 6780 WOLKRANGE Jolie maison d'habitation de plain-pied 3 façades " +
-          "(2 chambres), avec caves, abris de jardin, terrasse, garage, parking à l'avant du bâtiment et un jardin " +
-          "(OUEST) sur 9 ares et 26 centiares. Composition du bien : Rez-de-chaussée : hall d'entrée, WC séparé, " +
-          'vestiaire, vaste séjour lumineux (salon et salle à manger) avec accès à la terrasse, cuisine équipée, 2 ' +
-          'chambres, salle de bains (baignoire, meuble avec lavabo et un WC), buanderie et un garage (porte ' +
-          'sectionnelle électrique, un emplacement voiture). Combles : un spacieux grenier partiellement aménagé ' +
-          '(accès par un escalier escamotable). Sous-sol : chaufferie, coin douche et une réserve. Quelques ' +
-          'renseignements complémentaires : Châssis en bois avec double vitrage, chauffage central au mazout ' +
-          '(chaudière « Weishaupt » de 1995, cuve de 2.950 L), toiture en ardoises, volets, sols carrelés et ' +
-          'tapis plain, compteur électrique simple horaire, ... Superficie habitable : +/- 173 m² Travaux de ' +
-          'rafraichissements à prévoir ! Cette maison est située dans un quartier calme dans le charmant village ' +
-          "de Wolkrange et à proximité de la ville d'Arlon, des commerces/supermarchés, restaurants, écoles, " +
-          'médecins/pharmacies, banques, autoroutes E411/E25, et de nombreuses activités. Année de construction : ' +
-          '1974 Certificat PEB : CLASSE E û 393 kWh/m² R.C : 832 € Prix demandé : 260.000 €'
-      },
-      {
-        Type: 'Farmhouse',
-        City: 'Ruddervoorde',
-        PostalCode: 8020,
-        Bedrooms: 3,
-        Size: 19000,
-        Price: 1190000,
-        Duration: 65,
-        Image: '../../assets/housie2.jpg',
-        Info:
-          'Oostduinkerke – Zeer ruime boerderij met grote stal. Bel Eric, ook op zon-en feestdagen, ' +
-          '0475/82.11.11, of bezoek bureel BROKER Leopold II laan 83a OOSTDUINKERKE, recht tegenover "De Mikke” ' +
-          'met ruime parking.'
-      }
-    ];
     this.ngOnInit();
   }
 
@@ -91,11 +36,16 @@ export class HomeComponent implements OnInit, OnChanges {
           City: item.property.location.address.locality,
           PostalCode: item.property.location.address.postalCode,
           Bedrooms: item.property.bedroom.count,
-          Size: 178,
-          Price: 160000,
+          Size: Math.round(Math.random() * 10000),
+          Price: Math.round(Math.random() * 10000),
           Duration: item.travelDuration,
           Image: item.media.pictures.baseUrl + item.media.pictures.items[0].relativeUrl.large,
-          Info: ''
+          Info:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ' +
+            'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ' +
+            'ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+            'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa ' +
+            'qui officia deserunt mollit anim id est laborum.'
         };
       });
       console.log(results);

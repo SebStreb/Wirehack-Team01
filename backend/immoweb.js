7d3c48995a0b11b8744f511c8a989ea3e4f157ef
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2018-10-13 21:49:52 +0200

    commit before merge

--- backend/immoweb.js
+++ backend/immoweb.js
@@ -1,27 +1,40 @@
-const request = require('request-promise');
-const querystring = require('querystring');
-const polyline = require('@mapbox/polyline');
-const api = require('./api.json');
-
+const request = require("request-promise");
+const querystring = require("querystring");
+const polyline = require("@mapbox/polyline");
+const api = require("./api.json");
 
 // coordinates is a [latitude, longitude]
-exports.getClassifieds = async (coordinates) => {
+exports.getClassifieds = async coordinates => {
   const params = {
-    propertyTypes: 'HOUSE', // required
-    transactionTypes: 'FOR_RENT', // required
+    propertyTypes: "HOUSE", // required
+    transactionTypes: "FOR_RENT", // required
     minBedroomCount: 2,
     maxPrice: 1500,
     geoSearchPoint: polyline.encode([coordinates]),
     geoSearchRadius: 10000,
-    range: '0-30',
+    range: "0-30"
   };
+  const url = `${api.immoweb_url}/classifieds?${querystring.stringify(params)}`;
 
-  if (api.debug) console.log('GET', `${api.immoweb_url}/classifieds?${querystring.stringify(params)}`);
+  if (api.debug) console.log("GET", url);
   return request({
-    url: `${api.immoweb_url}/classifieds?${querystring.stringify(params)}`,
+    url: url,
     headers: {
-      'x-iw-api-key': api.immoweb_key,
-      Accept: 'application/vnd.be.immoweb.classifieds.v2.1+json',
-    },
+      "x-iw-api-key": api.immoweb_key,
+      Accept: "application/vnd.be.immoweb.classifieds.v2.1+json"
+    }
+  }).then(body => JSON.parse(body));
+};
+
+exports.getInformations = async classifiedID => {
+  const url = `${api.immoweb_url}/classifieds/${classifiedID}`;
+
+  if (api.debug) console.log("GET", url);
+  return request({
+    url: url,
+    headers: {
+      "x-iw-api-key": api.immoweb_key,
+      Accept: "application/vnd.be.immoweb.classifieds.v2.1+json"
+    }
   }).then(body => JSON.parse(body));
 };

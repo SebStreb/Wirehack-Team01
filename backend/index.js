0fd0793518fde8a475c8ce7ff5b16819adc05070
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2018-10-13 15:13:08 +0200

    google direction basis

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,16 @@ const polyline = require('@mapbox/polyline');
 //Line to encode lat/long in Google :
 //console.log(polyline.encode([[50.83711, 4.399754]]))
 
-const api = JSON.parse(fs.readFileSync("api.json"))
+const api = JSON.parse(fs.readFileSync('api.json'))
 
 const app = express()
 
-app.get('/', function (req, res) {
+function logError(error, response) {
+	console.error('error: ', error)
+	console.error('statusCode: ', response && response.statusCode)
+}
+
+app.get('/test1', (req, res) => {
 	const params = {
 		propertyTypes: 'HOUSE', //required
 		transactionTypes: 'FOR_RENT', //required
@@ -28,8 +33,26 @@ app.get('/', function (req, res) {
 			'Accept': 'application/vnd.be.immoweb.classifieds.v2.0+json' //required
 		}
 	}, function (error, response, body) {
-		if (error) console.log(error)
-		console.log(response)
+		if (error) logError(error, response)
+		console.log(body)
+		res.send(body)
+	})
+})
+
+app.get("/test2", (req, res) => {
+	const params = {
+		key: api.google_key,
+		origin: '50.837166,4.399113',
+		destination: '50.837971,4.390824',
+		mode: 'driving',
+		alternatives: false //we only need one route to get its time
+		//departure_time: TODO get timestamp for a random 8:00 in the future
+	}
+
+	request({
+		url: api.google_url + '/directions/json?' + querystring.stringify(params)
+	}, function (error, response, body) {
+		if (error) logError(error, response)
 		console.log(body)
 		res.send(body)
 	})

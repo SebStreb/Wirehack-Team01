67d03f2bf812068a9b89abec095fc97d63fbe741
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2018-10-14 12:09:17 +0200

    deleting old comments

--- backend/index.js
+++ backend/index.js
@@ -43,19 +43,6 @@ app.get('/get-location', async (req, res) => {
   if (!req.query.minSize || req.query.minSize.length === 0) minSize = '0';
   else minSize = req.query.minSize;
 
-  /* LEGACY
-  var multiple = false;
-  var otherCoordinates;
-  if (req.query.otherLoc !== "undefined") {
-    const otherLoc = req.query.otherLoc;
-    otherCoordinates = await google.getCoordinates(otherLoc);
-    console.log(otherLoc);
-    multiple = true;
-  }
-  */
-
-  console.log('1');
-
   const allCoordinates = await Promise.all(
     locations.map(async location => await google.getCoordinates(location)),
   );
@@ -77,8 +64,6 @@ app.get('/get-location', async (req, res) => {
     maxPrice,
   );
   const filtered = houses.filter(house => house.property.location.hasOwnProperty('geoPoint'));
-
-  console.log('2');
 
   const results = [];
   for await (const house of filtered) {
@@ -134,88 +119,8 @@ app.get('/get-location', async (req, res) => {
     results.push(result);
   }
 
-  console.log('3');
 
   return res.send(results);
-
-  /*
-  const promiseFiltered = filtered.map(async item => {
-    // do the google directions api call here to get duration
-    itemCoordinates = [
-      item.property.location.geoPoint.latitude,
-      item.property.location.geoPoint.longitude
-    ];
-
-    item.travelDuration = await google.getDuration(inputCoordinates, itemCoordinates);
-
-    if (multiple) {
-      item.travelDuration2 = {
-        driving: await google.getDuration(
-          otherCoordinates,
-          itemCoordinates,
-          "driving"
-        ),
-        walking: await google.getDuration(
-          otherCoordinates,
-          itemCoordinates,
-          "walking"
-        )
-      };
-    }
-
-    const detailedItem = await immoweb.getInformations(item.id);
-
-    if (detailedItem.property.hasOwnProperty("description"))
-      item.description = detailedItem.property.description;
-    else if (detailedItem.property.alternativeDescriptions.hasOwnProperty("en"))
-      item.description = detailedItem.property.alternativeDescriptions.en;
-    else if (detailedItem.property.alternativeDescriptions.hasOwnProperty("fr"))
-      item.description = detailedItem.property.alternativeDescriptions.fr;
-    else if (detailedItem.property.alternativeDescriptions.hasOwnProperty("nl"))
-      item.description = detailedItem.property.alternativeDescriptions.nl;
-
-    if (item.transaction.hasOwnProperty("sale"))
-      item.price = item.transaction.sale.price;
-    else if (item.transaction.hasOwnProperty("rental"))
-      item.price = item.transaction.rental.monthlyRentalPrice;
-    else item.price = "-1";
-
-    if (
-      item.property.hasOwnProperty("livingDescription") &&
-      item.property.livingDescription.hasOwnProperty("netHabitableSurface")
-    )
-      item.surface = item.property.livingDescription.netHabitableSurface;
-    else item.surface = "-1";
-
-    if (
-      item.property.hasOwnProperty("bedroom") &&
-      item.property.bedroom.hasOwnProperty("count")
-    )
-      item.bedrooms = item.property.bedroom.count;
-    else item.bedrooms = "-1";
-
-    return item;
-  });
-
-  const results = await Promise.all(promiseFiltered);
-
-  //TODO make all that in the frontend
-  //const cleanResults = results
-  // only include those that we can find a route to
-  //.filter(item => item.travelDuration != "-1")
-  // only include those where the duration is not too long
-  //.filter(item => parseInt(item.travelDuration.driving) < maxWait * 60)
-  // only include those that we can find a price to
-  //.filter(item => item.price != "-1") // TODO do we ?
-  // sort them by lowest travelroute
-  //.sort(
-  //  (a, b) =>
-  //    parseInt(a.travelDuration.driving, 10) -
-  //    parseInt(b.travelDuration.driving, 10)
-  //);
-
-  return res.send(results);
-  */
 });
 
 app.listen(3000, () => {
